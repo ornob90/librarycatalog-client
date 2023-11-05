253847870250d7863ff39add817d15b963653a90
ornob90 <oornob49@gmail.com>
2023-11-05 11:08:46 +0600

    switiching sie in the about section

--- src/pages/Home/About.jsx
+++ src/pages/Home/About.jsx
@@ -5,28 +5,7 @@ import Button from "../../components/Shared/Button";
 const About = () => {
   return (
     <Container className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 h-full  my-20 gap-8 md:gap-0">
-      <div
-        className="lg:col-span-3 flex flex-col gap-4 
-   justify-center text-center md:text-left"
-      >
-        <h3 className="font-medium text-[#808080] text-md text-sm">About Us</h3>
-        <h1 className="font-bold text-3xl sm:text-4xl md:text-3xl lg:text-5xl">
-          Explore Our Library
-        </h1>
-        <p className="text-[#808080] text-[13px] w-3/4 mx-auto md:m-0 lg:w-2/4 text-center md:text-left">
-          "Welcome to our library, a haven of literary treasures. We're
-          dedicated to fostering a love for reading, offering a diverse
-          collection spanning various genres and authors. Our mission is to
-          inspire, educate, and connect readers to a world of knowledge and
-          imagination"
-        </p>
-        <div>
-          <Button className="bg-black text-white py-1 px-6 md:py-2 md:px-8 rounded-sm text-sm md:text-base mt-2">
-            Explore
-          </Button>
-        </div>
-      </div>
-      <div className="h-full lg:col-span-2 flex gap-2 justify-center ">
+      <div className="h-full lg:col-span-3 flex gap-2 justify-left">
         <div className=" ">
           <div className="w-[150px] h-[200px] lg:w-[200px] lg:h-[250px]  mb-2">
             <img
@@ -60,6 +39,24 @@ const About = () => {
               className="h-full w-full object-cover"
             />
           </div>
+        </div>
+      </div>
+      <div className="lg:col-span-2 flex flex-col gap-4 justify-center text-center md:text-left">
+        <h3 className="font-medium text-[#808080] text-md text-sm">About Us</h3>
+        <h1 className="font-bold text-3xl sm:text-4xl md:text-3xl lg:text-5xl">
+          Explore Our Library
+        </h1>
+        <p className="text-[#808080] text-[13px] w-3/4 mx-auto md:m-0 lg:w-2/4 text-center md:text-left">
+          "Welcome to our library, a haven of literary treasures. We're
+          dedicated to fostering a love for reading, offering a diverse
+          collection spanning various genres and authors. Our mission is to
+          inspire, educate, and connect readers to a world of knowledge and
+          imagination"
+        </p>
+        <div>
+          <Button className="bg-black text-white py-1 px-6 md:py-2 md:px-8 rounded-sm text-sm md:text-base mt-2">
+            Explore
+          </Button>
         </div>
       </div>
     </Container>
---
+++ src/pages/Home/Testimonials.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "../../components/shared/Container";
+
+const Testimonials = () => {
+  return (
+    <Container>
+      <div>Testimonials</div>
+    </Container>
+  );
+};
+
+export default Testimonials;
